47427fba60a6c4406da4476d986095f200cc42b5
MuradTcen <oops-1.3@yandex.ru>
2016-11-14 12:33:28 +0500

    idk

--- task1.py
+++ task1.py
@@ -15,7 +15,7 @@ http://festival.1september.ru/articles/649134/
 A=0.3 
 B=0.75
 C=2
-R=((3*A*B*C)/(4*math.pi))**(3/2)
+R=((3*A*B*C)/(4*math.pi))**(1/3)
 V=A*B*C
 RO=1000
 G=9.8
@@ -33,11 +33,11 @@ def integr_trap(a,b,n,f):
 
 #-------------------- 4 CUBE \/
 def f_a(c):
-    return RO*G*c*B
+    return RO*G*c*B*c
     
     
 def f_b(с):
-    return RO*G*A*с
+    return RO*G*A*с*с
 
 
 def calc_pressure_cub_a_side(c):
@@ -49,15 +49,18 @@ def calc_pressure_cub_b_side(c):
     
     
 def calc_pressure_cub_bottom(a,b,c):
+
     return RO*G*a*b*c
 #-------------------- 4 CUBE /\
 
 #-------------------- 4 SPHERE \/
-get_rad=(lambda h: math.pi*(2*h*R-h*h)**0.5) 
-
+get_rad=(lambda h: math.pi*(2*h*R-h*h)*RO*G)
+get_df=(lambda x: math.pi*RO*G*x*(2*R-x)*(2*R-x))
+ 
+ 
 def calc_pressure_sphere_sides(R):
-    return spint.quad(get_rad,0,2*R)
-
+    #return spint.quad(get_rad,0,2*R)
+    return spint.quad(get_df,0,2*R)
 
 def calc_pressure_sphere_bottom(R):
     return RO*G*R
@@ -69,6 +72,6 @@ get_h=(lambda a,b,n: (b-a)/n)
 print ('bottom:',calc_pressure_cub_bottom(A,B,C)) 
 print ('a-side:',calc_pressure_cub_a_side(C))  
 print ('b-side',calc_pressure_cub_b_side(C))   
-print (calc_pressure_sphere_sides(R))
+print ('sphere pressure',calc_pressure_sphere_sides(R))
 
     
